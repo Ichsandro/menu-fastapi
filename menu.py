d5e6c78b2d16e99931717d4952b58d88f05084be
Ichsandro <ichsandro2001@gmail.com>
2021-10-16 15:26:04 +0700

    new file:   authentication.py 	modified:   menu.py 	modified:   requirements.txt 	new file:   user.json

--- menu.py
+++ menu.py
@@ -1,7 +1,8 @@
 # M Ichsandro D Noor 18219094
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 import json
+from authentication import AuthHandler
 
 def IsFound(id: int):
     with open("menu.json", "r") as read_file:
@@ -17,21 +18,52 @@ def IsFound(id: int):
 with open("menu.json", "r") as read_file:
     data = json.load(read_file)
 
+with open("user.json", "r") as read_file:
+    users = json.load(read_file)
+    
 app = FastAPI()
 
-@app.get('/menu')
-async def read_all_menu():
-	return data['menu'] 
+auth_handler = AuthHandler()
 
-@app.get('/menu/{item_id}')
-async def read_menu(item_id: int):
+@app.post('/register', status_code=201, tags=['User'])
+def register(username: str, password: str):
+    if any(x['username'] == username for x in users["user"]):
+        raise HTTPException(status_code=400, detail='Username is taken')
+    hashed_password = auth_handler.get_password_hash(password)
+    users["user"].append({
+        'username': username,
+        'password': hashed_password    
+    })
+    with open("user.json", "w") as write_file:
+            json.dump(users, write_file, indent = 3)
+    return {"username": username, "password": password}
+
+@app.post('/login', tags=['User'])
+def login(username: str, password: str):
+    user = None
+    for x in users["user"]:
+        if x['username'] == username:
+            user = x
+            break
+    
+    if (user is None) or (not auth_handler.verify_password(password, user['password'])):
+        raise HTTPException(status_code=401, detail='Invalid username and/or password')
+    token = auth_handler.encode_token(user['username'])
+    return { 'token': token }
+
+@app.get('/menu', tags=['Menu'])
+async def read_all_menu(current_user: users = Depends(auth_handler.auth_wrapper)):
+    return (data['menu'])
+
+@app.get('/menu/{item_id}', tags=['Menu'])
+async def read_menu(item_id: int, current_user: users = Depends(auth_handler.auth_wrapper)):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
             return menu_item
     raise HTTPException(status_code=404, detail="Item not found")
     
-@app.post('/menu')
-async def add_menu(item_id: int, name: str):
+@app.post('/menu', tags=['Menu'])
+async def add_menu(item_id: int, name: str, current_user: users = Depends(auth_handler.auth_wrapper)):
     if (IsFound(id = item_id)):
         raise HTTPException(status_code=404, detail="Item id is Used!")
     else:
@@ -41,8 +73,8 @@ async def add_menu(item_id: int, name: str):
             json.dump(data, write_file, indent = 3)
         return (item)
 
-@app.patch('/menu')
-async def update_menu(item_id: int, name: str):
+@app.patch('/menu', tags=['Menu'])
+async def update_menu(item_id: int, name: str,current_user: users = Depends(auth_handler.auth_wrapper)):
     if (IsFound(id = item_id)):
         x = {"id": item_id, "name": name}
         for menu_item in data['menu']:
@@ -54,8 +86,8 @@ async def update_menu(item_id: int, name: str):
     else:
         raise HTTPException(status_code=404, detail="Item not found!")
 
-@app.delete('/menu/{item_id}')
-async def delete_menu(item_id: int):
+@app.delete('/menu/{item_id}', tags=['Menu'])
+async def delete_menu(item_id: int, current_user: users = Depends(auth_handler.auth_wrapper)):
     if (IsFound(id = item_id)):
         for i in range(len(data['menu'])):
                 if data['menu'][i]['id'] == item_id:
